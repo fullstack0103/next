857f88bf3b1c9e179d01b6d4b544560f9326379f
fullstack0103 <fullstack0103@gmail.com>
2022-08-05 20:08:45 +0800

    Update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,28 +6,40 @@ import {
   WrapInput
 } from './styles'
 
-export const Home = () => {
+export const Home = ({ homeState }) => {
   const handleAddressInput = () => {
     console.log('address')
   }
 
+  console.log(homeState)
+
   return (
-    <HeroContainer bgimage='/home-hero.webp'>
-      <ContentWrapper>
-        <div>
-          <Title>All We need is Food.</Title>
-          <Slogan>Let us start to order food now</Slogan>
-          <WrapInput onClick={handleAddressInput} withIcon>
-            <p>What is your address?</p>
-          </WrapInput>
-          <button
-            name='find-business'
-            onClick={() => console.log('find')}
-          >
-            Find businesses
-          </button>
-        </div>
-      </ContentWrapper>
-    </HeroContainer>
+    <>
+      <HeroContainer bgimage='/home-hero.webp'>
+        <ContentWrapper>
+          <div>
+            <Title>All We need is Food.</Title>
+            <Slogan>Let us start to order food now</Slogan>
+            <WrapInput onClick={handleAddressInput} withIcon>
+              <p>What is your address?</p>
+            </WrapInput>
+            <button
+              name='find-business'
+              onClick={() => console.log('find')}
+            >
+              Find businesses
+            </button>
+          </div>
+        </ContentWrapper>
+      </HeroContainer>
+      {
+        homeState?.result?.body && (
+          <div dangerouslySetInnerHTML={{
+            __html: homeState?.result?.body
+          }}
+          />
+        )
+      }
+    </>
   )
 }
